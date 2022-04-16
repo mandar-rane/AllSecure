fd5b35c04d3408e8ebf9451adbf8cf56c8d99e26
mandar-rane <98955924+mandar-rane@users.noreply.github.com>
2022-04-17 01:21:37 +0530

    Update loc_behav.js

--- Women security/loc_behav.js
+++ Women security/loc_behav.js
@@ -36,14 +36,59 @@ function getLocationUpdate(){
 }
 
 */
+var coord = document.getElementById("coords");
+function unfade(coord) {
+   var op = 0.1;  
+   coord.style.display = "flex";
+   var timer = setInterval(function () {
+       if (op >= 1){
+           clearInterval(timer);
+       }
+       coord.style.opacity = op;
+       coord.style.filter = 'alpha(opacity=' + op * 100 + ")";
+       op += op * 0.1;
+   }, 50);
+}
+
+
+
+function fade(coord) {
+   var op = 1;  
+   var timer = setInterval(function () {
+       if (op <= 0.1){
+           clearInterval(timer);
+           coord.style.display = "none";
+       }
+       coord.style.opacity = op;
+       coord.style.filter = 'alpha(opacity=' + op * 100 + ")";
+       op -= op * 0.1;
+   }, 50);
+}
+
+var state = 0;
+
+
+
 
 function getLocationUpdate(){
-   var latitude = "1234567lt";
-   var longitude = "1234567lg";
-   lat.innerHTML = latitude;
-   long.innerHTML = longitude;
-   btn.setAttribute("value","Stop Sharing Location");
-   btn.style.backgroundColor = "#e52e2e";
-   btn.style.color = "white";
-
+   
+   if (state == 0){
+      var latitude = "1234567lt";
+      var longitude = "1234567lg";
+      lat.innerHTML = latitude;
+      long.innerHTML = longitude;
+      unfade(coord);
+      btn.setAttribute("value","Stop Sharing Location");
+      btn.style.backgroundColor = "#2EE59D";
+      btn.style.color = "white";
+      state = 1;
+   }
+   else{
+      fade(coord);
+      btn.setAttribute("value","Share Live Location");
+      btn.style.backgroundColor = "#e52e2e";
+      btn.style.color = "white";
+      state = 0;
+   }
+   
 }
