06fa643fa5c82d9cae1ea445d1390c3c8ae973a2
mandar-rane <98955924+mandar-rane@users.noreply.github.com>
2022-04-26 23:40:55 +0530

    Update loc_behav.js

--- Women security/loc_behav.js
+++ Women security/loc_behav.js
@@ -4,38 +4,8 @@ var geoLoc;
 var lat = document.getElementById("lat");
 var long = document.getElementById("long");
 var btn = document.getElementById("btn");
+var liv = document.getElementById("liv");
 
-/*function showLocation(position) {
-   document.getElementById("coords").style.display = "flex"; 
-   var latitude = position.coords.latitude;
-   var longitude = position.coords.longitude;
-   lat.innerHTML = latitude;
-   lat.innerHTML = longitude;
-}
-
-
-function errorHandler(err) {
-   if(err.code == 1) {
-      alert("Error: Access is denied!");
-   } else if( err.code == 2) {
-      alert("Error: Position is unavailable!");
-   }
-}
-
-function getLocationUpdate(){
-   
-   if(navigator.geolocation){
-      
-      // timeout at 60000 milliseconds (60 seconds)
-      var options = {timeout:60000};
-      geoLoc = navigator.geolocation;
-      watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
-   } else {
-      alert("Sorry, browser does not support geolocation!");
-   }
-}
-
-*/
 var coord = document.getElementById("coords");
 function unfade(coord) {
    var op = 0.1;  
@@ -49,6 +19,7 @@ function unfade(coord) {
        op += op * 0.1;
    }, 50);
 }
+
 
 
 
@@ -80,7 +51,8 @@ function getLocationUpdate(){
       unfade(coord);
       btn.setAttribute("value","Stop Sharing Location");
       btn.style.backgroundColor = "#2EE59D";
-      btn.style.color = "white";
+      btn.style.color = "black";
+      liv.style.display = "flex";
       state = 1;
    }
    else{
@@ -88,6 +60,7 @@ function getLocationUpdate(){
       btn.setAttribute("value","Share Live Location");
       btn.style.backgroundColor = "#e52e2e";
       btn.style.color = "white";
+      liv.style.display = "none";
       state = 0;
    }
    
